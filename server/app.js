25960deac85f18f329484a9f36a3d43d7771c478
Keijo Kapp <keijo.kapp@gmail.com>
2015-08-10 11:53:07 +0300

    c

--- server/app.js
+++ server/app.js
@@ -1,24 +1,21 @@
 import util from 'util';
 import DhcpServer from './server';
 
-var bindings = [
-	{ addr: '0.0.0.0', port: 67 },
-];
+var bindings = { addr: '0.0.0.0', port: 67 };
  
 
-for(var i = 0; i < bindings.length; i++) {
-	var server = new DhcpServer;
+var server = new DhcpServer;
 
-	server.on('message', function(m) {
-		console.log('message: ', '(' + bindings[i].addr + ')', m.ciaddr)
-	});
+server.on('message', function(m) {
+	console.log('message: ', '(' + binding.addr + ')', m.ciaddr)
+});
 
-	server.on('listening', function(addr) {
-		console.log('listening on ' + addr.addr + ':' + addr.port);
-	});
+server.on('listening', function(addr) {
+	console.log('listening on ' + addr.addr + ':' + addr.port);
+});
 
-	server.bind(bindings[i])
-	.then(function() {
+server.bind(binding)
+.then(function() {
 //		console.log('bound to ' + bindints.addr + ':' + bindTo.port);
-	});
-}
+});
+
